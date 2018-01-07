ad136041bf695f5ed37a77665caab2cc81c670c9
andy <1>
2018-01-07 21:52:52 +0800

    增加

---
+++ app/src/main/java/com/bitmapregion/LargeView.java
@@ -0,0 +1,91 @@
+package com.bitmapregion;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by andy on 2018/1/6.
+ */
+
+public class LargeView extends View {
+
+    private BitmapRegionDecoder mDecoder;
+    /**
+     * 图片的宽度和高度
+     */
+    private int mImageWidth, mImageHeight;
+    /**
+     * 绘制的区域
+     */
+    private volatile Rect mRect = new Rect();
+
+    private static final BitmapFactory.Options options = new BitmapFactory.Options();
+
+
+    public LargeView(Context context) {
+        super(context);
+    }
+
+    public LargeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LargeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    public void setInputStream(InputStream is) {
+        try {
+            mDecoder = BitmapRegionDecoder.newInstance(is, false);
+            BitmapFactory.Options tmpOptions = new BitmapFactory.Options();
+            tmpOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(is, null, tmpOptions);
+            mImageWidth = tmpOptions.outWidth;
+            mImageHeight = tmpOptions.outHeight;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                if (is != null) is.close();
+            } catch (Exception e) {
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Bitmap bm = mDecoder.decodeRegion(mRect, options);
+        canvas.drawBitmap(bm, 0, 0, null);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+
+        int imageWidth = mImageWidth;
+        int imageHeight = mImageHeight;
+
+        //默认直接显示图片的中心区域，可以自己去调节
+        mRect.left = imageWidth / 2 - width / 2;
+        mRect.top = imageHeight / 2 - height / 2;
+        mRect.right = mRect.left + width;
+        mRect.bottom = mRect.top + height;
+
+    }
+}
--- app/src/main/java/com/bitmapregion/MainActivity.java
+++ app/src/main/java/com/bitmapregion/MainActivity.java
@@ -1,13 +1,49 @@
 package com.bitmapregion;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv = null;
+    private LargeView large = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.iv);
+        large = findViewById(R.id.large);
+
+        try {
+            InputStream inputStream = getAssets().open("timg.jpg");
+
+
+            large.setInputStream(inputStream);
+            //获得图片的宽、高
+//            BitmapFactory.Options tmpOptions = new BitmapFactory.Options();
+//            tmpOptions.inJustDecodeBounds = true;
+//            BitmapFactory.decodeStream(inputStream, null, tmpOptions);
+//            int width = tmpOptions.outWidth;
+//            int height = tmpOptions.outHeight;
+//
+//            //设置显示图片的中心区域
+//            BitmapRegionDecoder bitmapRegionDecoder = BitmapRegionDecoder.newInstance(inputStream, false);
+//            BitmapFactory.Options options = new BitmapFactory.Options();
+//            options.inPreferredConfig = Bitmap.Config.RGB_565;
+//            Bitmap bitmap = bitmapRegionDecoder.decodeRegion(new Rect(width / 2 - 100, height / 2 - 100, width / 2 + 100, height / 2 + 100), options);
+//            iv.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
